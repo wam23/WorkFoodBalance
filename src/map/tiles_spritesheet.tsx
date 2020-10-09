c5f28bdfe06c3f6eb429696f96e3f0ed2a6c52e1
danielabodmer <68418201+danielabodmer@users.noreply.github.com>
2020-10-09 18:19:47 +0200

    newest images and icons

--- src/map/tiles_spritesheet.tsx
+++ src/map/tiles_spritesheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="tiles_spritesheet" tilewidth="72" tileheight="72" tilecount="156" columns="12">
- <image source="../tiles_spritesheet-1.png" width="914" height="936"/>
+ <image source="../WorkFoodBalance-main/dist/assets/tiles_spritesheet.png" width="914" height="936"/>
 </tileset>
