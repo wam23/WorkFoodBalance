273141d76cd4d28d562815b8b6db9f367d7cf91a
Rafael Wampfler <wampfler@gmail.com>
2020-10-10 00:47:14 +0200

    score screen

--- src/scenes/ScoreScene.js
+++ src/scenes/ScoreScene.js
@@ -11,10 +11,10 @@ export class ScoreScreen extends Phaser.Scene {
     create () {
         this.add.image(2500, 360,"sky_level1");
 
-        this.foreverText = this.add.text(100, 150, '0', { fontSize: '150px', fill: '#ffcf00' });
+        this.foreverText = this.add.text(250, 140, '0', { fontSize: '120px', fill: '#ffcf00' });
         this.foreverText.setText(this.game.forever.join(''));
 
-        let nextButton = this.add.image(400, 400, 'play_button');
+        let nextButton = this.add.image(1100, 400, 'play_button');
         nextButton.setInteractive();
 
         nextButton.on("pointerup", () => {
