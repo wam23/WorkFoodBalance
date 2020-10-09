8c492083b81f82c66c373b0268f0bf2da6c07e23
hawkie23 <hawkie@bluewin.ch>
2020-10-09 22:29:57 +0200

    .

--- src/scenes/Level2Scene.js
+++ src/scenes/Level2Scene.js
@@ -1,17 +1,17 @@
 import { CST } from "../CST.js"
 import { AbstractLevelScene } from "./AbstractLevelScene.js";
 
-export class Level1Scene extends AbstractLevelScene {
+export class Level2Scene extends AbstractLevelScene {
     
     constructor() {
-        super(CST.SCENES.LEVEL1);
+        super(CST.SCENES.LEVEL2);
     }
 
     create () {
         super.create();
 
         this.map = this.make.tilemap({
-            key: 'Level1',
+            key: 'Level2',
             tileWidth: 72,
             tileHeight: 72
         });
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -13,27 +13,20 @@ export class MenuScene extends Phaser.Scene {
     }
 
     create () {
-        //  A simple background for our game
         this.add.image(2500, 360,"sky");
 
-        let playIcon = this.add.sprite(300, 400, 'play_icon');
-        playIcon.setVisible(false);
-
-        // use as play button...
         let playButton = this.add.image(400, 400, 'play_button');
-
         playButton.setInteractive();
 
         playButton.on("pointerup", () => {
             this.scene.start(CST.SCENES.LEVEL1);
         });
 
-        playButton.on("pointerover", () => {
-            playIcon.setVisible(true);
-        });
+        let playButton2 = this.add.image(400, 500, 'play_button');
+        playButton2.setInteractive();
 
-        playButton.on("pointerout", () => {
-            playIcon.setVisible(false);
+        playButton2.on("pointerup", () => {
+            this.scene.start(CST.SCENES.LEVEL2);
         });
 
     }
